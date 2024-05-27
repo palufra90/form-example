f5c0f881dd613ac34bd447a1bad0552b9853649f
Francesca <palu.fra90@gmail.com>
2024-05-27 16:21:13 +0200

    test: try with nanostore in astro component

--- src/components/FormState.tsx
+++ src/components/FormState.tsx
@@ -3,8 +3,8 @@ import { formState } from "./form.store";
 
 export default function FormState() {
   const [userName, setUsersName] = React.useState<string>(" ");
-
   const [email, setEmail] = React.useState<string>("");
+  
   formState.listen(() => {
     setEmail(formState.get().email);
     setUsersName(formState.get().userName);
