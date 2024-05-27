6cd8a20d22bc3c04c0987b8306ebbb929f61e91f
Francesca <palu.fra90@gmail.com>
2024-05-27 17:45:27 +0200

    refactor: simplify state astro component

--- src/components/FormState.tsx
+++ src/components/FormState.tsx
@@ -4,12 +4,12 @@ import { formState } from "./form.store";
 export default function FormState() {
   const [userName, setUsersName] = React.useState<string>(" ");
   const [email, setEmail] = React.useState<string>("");
-  
+
   formState.listen(() => {
     setEmail(formState.get().email);
     setUsersName(formState.get().userName);
-    console.log("form state changed", { userName, email });
   });
+
   return (
     <>
       <h4>Form state</h4>
